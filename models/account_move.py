6e91cf3cf98244934bff9bded37af05c82aec875
kingyond <900483@qq.com>
2025-07-13 15:42:46 +0800

    修正odoo 18 的i18n

--- models/account_move.py
+++ models/account_move.py
@@ -3,37 +3,12 @@ from odoo import models, fields, api, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
     
-    # 发票类型选项
-    CN_FAPIAO_TYPE_SELECTION = [
-        ('special', '专用发票'),
-        ('normal', '普通发票'),
-        ('none', '无需开票'),
-    ]
-
-    # 更改翻译的方式：不在字段定义时使用_()
-    cn_fapiao_type = fields.Selection(
-        CN_FAPIAO_TYPE_SELECTION,
-        string='发票类型',
-        tracking=True,
-    )
-    cn_fapiao_code = fields.Char(
-        string='发票代码',
-        tracking=True,
-    )
-    cn_fapiao_number = fields.Char(
-        string='发票编号',
-        tracking=True,
-    )
+    # 添加中国发票相关字段，不修改原有定义
+    cn_fapiao_type = fields.Selection([
+        ('none', _('No Invoice Required')),
+        ('special', _('Special VAT Invoice')),
+        ('general', _('General VAT Invoice'))
+    ], string=_('Invoice Type'), default='none', tracking=True)
     
-    # 在需要翻译的方法中使用_()函数
-    @api.onchange('cn_fapiao_type')
-    def _onchange_cn_fapiao_type(self):
-        if self.cn_fapiao_type == 'none':
-            self.cn_fapiao_code = False
-            self.cn_fapiao_number = False
-            return {
-                'warning': {
-                    'title': _('提示'),
-                    'message': _('已选择"无需开票"，发票代码和编号已清空')
-                }
-            }
+    cn_fapiao_code = fields.Char(string=_('Invoice Code'), tracking=True)
+    cn_fapiao_number = fields.Char(string=_('Invoice Number'), tracking=True)
